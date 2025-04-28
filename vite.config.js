6c75be7a74f54b605b18a48ac4322f2d752b1da2
ed-vin <edvinullberg@outlook.com>
2025-04-28 15:40:20 +0200

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cakery-react-web-site/', // <- Ditt nya repo-namn
+  base: '/cakery-react-web-site/', // <-- detta är korrekt för länken https://ed-vin.github.io/cakery-react-web-site/
   plugins: [react()],
 });
